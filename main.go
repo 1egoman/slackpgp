dbb7bac7c18478a4672dc4e99334f6b2e79bcbf1
Ryan Gaus <rsg1egoman@gmail.com>
2017-04-18 19:42:35 -0400

    code: format css

--- main.go
+++ main.go
@@ -101,72 +101,75 @@ func WebhookHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func InfoHandler(w http.ResponseWriter, r *http.Request) {
-  // NOTE: handler ?key=1 redirects after setting pub key
   io.WriteString(w, `<html>
     <head>
       <title>PGP Slackbot</title>
       <style>
-      body {
-        font-family: Helvetica, Arial, sans-serif;
-      }
+        body {
+          font-family: Helvetica, Arial, sans-serif;
+          padding: 20px;
+        }
 
-      /* Hide sections that come in the future */
-      .step-post-team-name, .step-post-slash-command, .step-complete {
-        margin-top: 2em;
-        margin-bottom: 1em;
-        min-height: 200px;
-        display: none;
-      }
+        /* Hide sections that come in the future */
+        #step-post-team-name, #step-post-slash-command, #step-complete {
+          margin-top: 2em;
+          margin-bottom: 1em;
+          min-height: 200px;
+          display: none;
+        }
 
-      button {
-        display: block;
-        font-size: 1.4em;
-        background-color: #2ab27b;
-        color: #ffffff;
-        padding: 10px 20px;
-        border: 0px;
-        border-radius: 4px;
-        margin-top: 1em;
-        outline: none;
-      }
-      button:active {
-        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3) inset;
-      }
+        button {
+          display: block;
+          font-size: 1.4em;
+          background-color: #2ab27b;
+          color: #ffffff;
+          padding: 10px 20px;
+          border: 0px;
+          border-radius: 4px;
+          margin-top: 1em;
+          outline: none;
+          cursor: pointer;
+        }
+        button:active {
+          box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.3) inset;
+        }
 
-      input[type=text] {
-        padding: 2px 4px;
-      }
+        input[type=text] {
+          padding: 2px 4px;
+        }
 
-      .warning {
-        color: #EDB431;
-      }
+        .warning {
+          color: #EDB431;
+        }
 
-      code {
-        padding: 2px 4px;
-        font-size: 90%;
-        color: #fff;
-        background-color: #333;
-        border-radius: 3px;
-        -webkit-box-shadow: inset 0 -1px 0 rgba(0,0,0,.25);
-        box-shadow: inset 0 -1px 0 rgba(0,0,0,.25);
-      }
+        code {
+          padding: 2px 4px;
+          font-size: 90%;
+          color: #fff;
+          background-color: #333;
+          border-radius: 3px;
+          -webkit-box-shadow: inset 0 -1px 0 rgba(0,0,0,.25);
+          box-shadow: inset 0 -1px 0 rgba(0,0,0,.25);
+        }
 
-      li {
-        padding: 2px 0px;
-      }
+        li {
+          padding: 2px 0px;
+        }
       </style>
     </head>
     <body>
       <h1>Welcome to the PGP Slackbot!</h1>
       This slackbot provides a simple way to send encrypted messages with PGP.
       
-      <h2>Setup</h2>
-      <p>
-        First, what's your slack team url? <input type="text" id="slack-team-name" /><code>.slack.com</code>
-        <button id="slack-team-continue">Continue</button>
-      </p>
+      <span id="step-initial">
+        <h2>Setup</h2>
+        <p>
+          First, what's your slack team url? <input type="text" id="slack-team-name" /><code>.slack.com</code>
+          <button id="slack-team-continue">Continue</button>
+        </p>
+      </span>
 
-      <span class="step-post-team-name">
+      <span id="step-post-team-name">
         <h2>Setup slash command</h2>
         <p>
           <a id='slack-slash-command-setup' target="_blank" href="#dynamic-data-here">Click here</a> to setup a slash command for your team.
@@ -178,7 +181,7 @@ func InfoHandler(w http.ResponseWriter, r *http.Request) {
         <button id="slack-slash-command-continue">Continue</button>
       </span>
 
-      <span class="step-post-slash-command">
+      <span id="step-post-slash-command">
         <h2>Configure slash command</h2>
         Confirm the following textboxes are filled in:
         <ul>
@@ -191,7 +194,7 @@ func InfoHandler(w http.ResponseWriter, r *http.Request) {
         <button id="slack-bot-created-continue">Continue</button>
       </span>
 
-      <span class="step-complete">
+      <span id="step-complete">
         <h2>Test the bot</h2>
         <p>
           Now that the bot is set up, try running this command: <code>/pgp init</code>
@@ -211,32 +214,23 @@ func InfoHandler(w http.ResponseWriter, r *http.Request) {
         var teamName = document.getElementById('slack-team-name').value;
         document.getElementById('slack-slash-command-setup').href = "https://"+teamName+".slack.com/apps/new/A0F82E8CA"
 
-        // Show the next steps.
-        var elements = document.getElementsByClassName('step-post-team-name');
-
-        for (var i = 0; i < elements.length; i++) {
-          elements[i].style.display = 'block';
-        }
+        // Show the next step, and hide the previous step.
+        document.getElementById('step-initial').style.display = 'none';
+        document.getElementById('step-post-team-name').style.display = 'block';
       }
 
       document.getElementById('slack-slash-command-continue').onclick = function() {
         document.getElementById('slack-command-hook-url').innerHTML = location.origin + "/webhook";
 
-        // Show the next steps.
-        var elements = document.getElementsByClassName('step-post-slash-command');
-
-        for (var i = 0; i < elements.length; i++) {
-          elements[i].style.display = 'block';
-        }
+        // Hide previous step, show next step.
+        document.getElementById('step-post-team-name').style.display = 'none';
+        document.getElementById('step-post-slash-command').style.display = 'block';
       }
 
       document.getElementById('slack-bot-created-continue').onclick = function() {
-        // Show the next steps.
-        var elements = document.getElementsByClassName('step-complete');
-
-        for (var i = 0; i < elements.length; i++) {
-          elements[i].style.display = 'block';
-        }
+        // Hide previous step, show next step.
+        document.getElementById('step-post-slash-command').style.display = 'none';
+        document.getElementById('step-complete').style.display = 'block';
       }
       </script>
     </body>
