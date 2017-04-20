4b277e1bd5978a545e6f34db2fba44b06eaf9a9e
Ryan Gaus <rsg1egoman@gmail.com>
2017-04-20 07:33:22 -0400

    code: go fmt

--- main.go
+++ main.go
@@ -1,140 +1,140 @@
 package main
 
 import (
-  "fmt"
-  "io"
-  "os"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
 
-  "bytes"
-  "strings"
+	"bytes"
+	"strings"
 
-  "net/http"
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+	"net/http"
 
-  // Contains our user model and database logic
-  "github.com/1egoman/slackpgp/users"
+	// Contains our user model and database logic
+	"github.com/1egoman/slackpgp/users"
 )
 
 func main() {
-  router := mux.NewRouter().StrictSlash(false)
+	router := mux.NewRouter().StrictSlash(false)
 
-  // Provide information when hitting the default route.
-  router.HandleFunc("/", InfoHandler).Methods("GET")
+	// Provide information when hitting the default route.
+	router.HandleFunc("/", InfoHandler).Methods("GET")
 
-  // Setting up a new user's public key
-  router.HandleFunc("/onboard/{secret}", OnboardTemplateHandler).Methods("GET")
-  router.HandleFunc("/onboard/{secret}", OnboardHandler).Methods("POST")
-  router.HandleFunc("/onboard_success", UserEnteredPubKeyHandler).Methods("GET")
+	// Setting up a new user's public key
+	router.HandleFunc("/onboard/{secret}", OnboardTemplateHandler).Methods("GET")
+	router.HandleFunc("/onboard/{secret}", OnboardHandler).Methods("POST")
+	router.HandleFunc("/onboard_success", UserEnteredPubKeyHandler).Methods("GET")
 
-  // The main entrypoint - the slack webhook.
-  router.HandleFunc("/webhook", WebhookHandler).Methods("POST")
+	// The main entrypoint - the slack webhook.
+	router.HandleFunc("/webhook", WebhookHandler).Methods("POST")
 
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8000"
-  }
-  fmt.Println("Listening on :"+port)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	fmt.Println("Listening on :" + port)
 	http.ListenAndServe(":"+port, router)
 }
 
 func WebhookHandler(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
+	r.ParseForm()
 
-  if len(r.Form["text"]) == 0 {
-    io.WriteString(w, "No text arg!")
-    return
-  }
-  if len(r.Form["user_name"]) == 0 {
-    io.WriteString(w, "No user_name arg!")
-    return
-  }
+	if len(r.Form["text"]) == 0 {
+		io.WriteString(w, "No text arg!")
+		return
+	}
+	if len(r.Form["user_name"]) == 0 {
+		io.WriteString(w, "No user_name arg!")
+		return
+	}
 
-  // If a slack token was specified, validate against that.
-  if slack_token := os.Getenv("SLACK_VERIFICATION_TOKEN"); slack_token != "" && slack_token != r.Form["token"][0] {
-    io.WriteString(w, "Invalid slack token. Please make sure the SLACK_VERIFICATION_TOKEN environment variable is set correctly.")
-  }
+	// If a slack token was specified, validate against that.
+	if slack_token := os.Getenv("SLACK_VERIFICATION_TOKEN"); slack_token != "" && slack_token != r.Form["token"][0] {
+		io.WriteString(w, "Invalid slack token. Please make sure the SLACK_VERIFICATION_TOKEN environment variable is set correctly.")
+	}
 
-  senderUsername := r.Form["user_name"][0]
-  slashCommandText := r.Form["text"][0]
-  slashCommandPayload := strings.Split(slashCommandText, " ")
+	senderUsername := r.Form["user_name"][0]
+	slashCommandText := r.Form["text"][0]
+	slashCommandPayload := strings.Split(slashCommandText, " ")
 
-  // Did the user type anything after the slash command?
-  // ie, if the user just typed /pgp, then give them an error.
-  if len(strings.Trim(strings.Join(slashCommandPayload, ""), " ")) == 0 {
-    // Get the command that the user typed
-    command := "/pgp"
-    if len(r.Form["command"]) > 0 {
-      command = r.Form["command"][0]
-    }
+	// Did the user type anything after the slash command?
+	// ie, if the user just typed /pgp, then give them an error.
+	if len(strings.Trim(strings.Join(slashCommandPayload, ""), " ")) == 0 {
+		// Get the command that the user typed
+		command := "/pgp"
+		if len(r.Form["command"]) > 0 {
+			command = r.Form["command"][0]
+		}
 
-    // Send them an error
-    w.WriteHeader(404)
-    io.WriteString(w, "Please specify a command (ie, `"+command+" init`) or send a message (ie, `"+command+" @user my secret message`)")
-    return
-  }
+		// Send them an error
+		w.WriteHeader(404)
+		io.WriteString(w, "Please specify a command (ie, `"+command+" init`) or send a message (ie, `"+command+" @user my secret message`)")
+		return
+	}
 
-   switch r.Form["text"][0] {
-     // Setup a new user.
-     case "init":
-      u := users.NewUser(senderUsername)
-      u.EnableConfiguration()
-      u.Create()
+	switch r.Form["text"][0] {
+	// Setup a new user.
+	case "init":
+		u := users.NewUser(senderUsername)
+		u.EnableConfiguration()
+		u.Create()
 
-      // Where is the server hosted?
-      hostname := os.Getenv("HOSTNAME")
-      if hostname == "" {
-        hostname = "http://localhost:8000"
-      }
+		// Where is the server hosted?
+		hostname := os.Getenv("HOSTNAME")
+		if hostname == "" {
+			hostname = "http://localhost:8000"
+		}
 
-      // Give the user a path to set their public key.
-      io.WriteString(w, "Click here to configure your public key: "+hostname+"/onboard/"+u.Secret)
+		// Give the user a path to set their public key.
+		io.WriteString(w, "Click here to configure your public key: "+hostname+"/onboard/"+u.Secret)
 
-    // Send an encrypted message to another user.
-    default:
-      // Remove '@' from start of username string
-      recipientUsername := slashCommandPayload[0]
-      if recipientUsername[0] == '@' {
-        recipientUsername = recipientUsername[1:]
-      }
+	// Send an encrypted message to another user.
+	default:
+		// Remove '@' from start of username string
+		recipientUsername := slashCommandPayload[0]
+		if recipientUsername[0] == '@' {
+			recipientUsername = recipientUsername[1:]
+		}
 
-      recipient, err := users.GetUserByUsername(recipientUsername)
-      if recipient == nil {
-        io.WriteString(w, "The user "+slashCommandPayload[0]+" doesn't exist or hasn't registered. Tell them to run `/pgp init`.")
-        return
-      } else if err != nil {
-        io.WriteString(w, err.Error())
-      }
+		recipient, err := users.GetUserByUsername(recipientUsername)
+		if recipient == nil {
+			io.WriteString(w, "The user "+slashCommandPayload[0]+" doesn't exist or hasn't registered. Tell them to run `/pgp init`.")
+			return
+		} else if err != nil {
+			io.WriteString(w, err.Error())
+		}
 
-      // Send a placeholder response. This is so the slash command (with the private message!) won't
-      // be shown to everyone.  Later, we'll send the actual message async (with the `response_url`
-      // in the payload) which will be shown to everyone.
-      io.WriteString(w, " ")
+		// Send a placeholder response. This is so the slash command (with the private message!) won't
+		// be shown to everyone.  Later, we'll send the actual message async (with the `response_url`
+		// in the payload) which will be shown to everyone.
+		io.WriteString(w, " ")
 
-      // Encrypt a message.
-      message := strings.Join(slashCommandPayload[1:], " ")
-      encryptedMessageBody := map[string]interface{}{
-        "response_type": "in_channel",
-        "text": fmt.Sprintf(
-          "Hey <@%s>, here's a message from <@%s>: \n ```%s```",
-          recipientUsername,
-          senderUsername,
-          recipient.Encrypt(message),
-        ),
-      }
-      encodedEncryptedMessageBody, _ := json.Marshal(encryptedMessageBody)
+		// Encrypt a message.
+		message := strings.Join(slashCommandPayload[1:], " ")
+		encryptedMessageBody := map[string]interface{}{
+			"response_type": "in_channel",
+			"text": fmt.Sprintf(
+				"Hey <@%s>, here's a message from <@%s>: \n ```%s```",
+				recipientUsername,
+				senderUsername,
+				recipient.Encrypt(message),
+			),
+		}
+		encodedEncryptedMessageBody, _ := json.Marshal(encryptedMessageBody)
 
-      // Send encrypted message as an async message. See above on why this trick is required.
-      _, err = http.Post(
-        r.Form["response_url"][0],
-        "application/json",
-        bytes.NewBuffer([]byte(encodedEncryptedMessageBody)),
-      )
-  }
+		// Send encrypted message as an async message. See above on why this trick is required.
+		_, err = http.Post(
+			r.Form["response_url"][0],
+			"application/json",
+			bytes.NewBuffer([]byte(encodedEncryptedMessageBody)),
+		)
+	}
 }
 
 func InfoHandler(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, `<html>
+	io.WriteString(w, `<html>
     <head>
       <title>PGP Slackbot</title>
       <style>
@@ -271,6 +271,6 @@ func InfoHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserEnteredPubKeyHandler(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, `Cool, thanks for adding your public key!
+	io.WriteString(w, `Cool, thanks for adding your public key!
 Now, try to send someone else a message: /pgp @another-user my secret message!`)
 }
--- onboard.go
+++ onboard.go
@@ -1,73 +1,72 @@
 package main
 
 import (
-  "io"
+	"io"
 
-  "strings"
+	"strings"
 
-  "net/http"
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+	"net/http"
 
-  "github.com/1egoman/slackpgp/users"
+	"github.com/1egoman/slackpgp/users"
 )
-
 
 // This is called on GET /onboard/{secret}. It renders the form for the user to set their public
 // key.
 func OnboardTemplateHandler(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  user, err := users.GetUserBySecret(vars["secret"])
+	vars := mux.Vars(r)
+	user, err := users.GetUserBySecret(vars["secret"])
 
-  if err != nil {
-    // On error, print out the error as a response
-    w.WriteHeader(500)
-    w.Header().Set("Content-Type", "text/plain")
-    io.WriteString(w, err.Error())
-  } else if user != nil && user.IsConfigurable {
-    // We got a user that is configurable, so display a config page.
-    w.Header().Set("Content-Type", "text/html")
-    formattedTemplate := strings.Replace(ONBOARD_TEMPLATE, "{username}", user.Username, -1)
-    io.WriteString(w, formattedTemplate)
-  } else {
-    // User isn't configurable. Respond with 404.
-    w.WriteHeader(404)
-    io.WriteString(w, "No such user.")
-  }
+	if err != nil {
+		// On error, print out the error as a response
+		w.WriteHeader(500)
+		w.Header().Set("Content-Type", "text/plain")
+		io.WriteString(w, err.Error())
+	} else if user != nil && user.IsConfigurable {
+		// We got a user that is configurable, so display a config page.
+		w.Header().Set("Content-Type", "text/html")
+		formattedTemplate := strings.Replace(ONBOARD_TEMPLATE, "{username}", user.Username, -1)
+		io.WriteString(w, formattedTemplate)
+	} else {
+		// User isn't configurable. Respond with 404.
+		w.WriteHeader(404)
+		io.WriteString(w, "No such user.")
+	}
 }
 
 // This posts the new public key to a user with the given secret. This also resets the
 // `IsConfigurable` flag so the public key can't be reset uness the user initiates anotehr reset.
 func OnboardHandler(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  user, err := users.GetUserBySecret(vars["secret"])
+	vars := mux.Vars(r)
+	user, err := users.GetUserBySecret(vars["secret"])
 
-  if err == nil && user != nil {
-    // User with the specified secret key was found.
+	if err == nil && user != nil {
+		// User with the specified secret key was found.
 
-    // call to ParseForm makes form fields available.
-    err := r.ParseForm()
-    if err != nil {
-      w.Header().Set("Content-Type", "text/plain")
-      io.WriteString(w, err.Error())
-    }
+		// call to ParseForm makes form fields available.
+		err := r.ParseForm()
+		if err != nil {
+			w.Header().Set("Content-Type", "text/plain")
+			io.WriteString(w, err.Error())
+		}
 
-    // Update user public key, and block further configuration
-    user.PublicKey = r.PostFormValue("key")
-    user.IsConfigurable = false
-    user.Save()
+		// Update user public key, and block further configuration
+		user.PublicKey = r.PostFormValue("key")
+		user.IsConfigurable = false
+		user.Save()
 
-    // Redirect back to the root.
-    http.Redirect(w, r, "/onboard_success", 302)
-  } else if err == nil && user == nil {
-    // No user exists with that secret key
-    w.WriteHeader(404)
-    io.WriteString(w, "No such user.")
-  } else {
-    // Some other error occured.
-    w.WriteHeader(500)
-    w.Header().Set("Content-Type", "text/plain")
-    io.WriteString(w, err.Error())
-  }
+		// Redirect back to the root.
+		http.Redirect(w, r, "/onboard_success", 302)
+	} else if err == nil && user == nil {
+		// No user exists with that secret key
+		w.WriteHeader(404)
+		io.WriteString(w, "No such user.")
+	} else {
+		// Some other error occured.
+		w.WriteHeader(500)
+		w.Header().Set("Content-Type", "text/plain")
+		io.WriteString(w, err.Error())
+	}
 }
 
 const ONBOARD_TEMPLATE = `
